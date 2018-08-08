d5a923203fda39f10b8022ad534c84679eff3a54
Dennis Meelis <dennis.meelis@gmail.com>
2018-08-08 08:16:58 +0200

    Removed unused functions and logging

--- Assets/Scripts/Puzzle/PuzzlePart.cs
+++ Assets/Scripts/Puzzle/PuzzlePart.cs
@@ -19,42 +19,6 @@ public class PuzzlePart : MonoBehaviour {
   }
 
   Hand _hand;
-  
-		//-------------------------------------------------
-		// Called when this GameObject becomes attached to the hand
-		//-------------------------------------------------
-		private void OnAttachedToHand( Hand hand )
-		{
-			Debug.Log("Attached to hand " + hand.name);
-		}
-
-
-		//-------------------------------------------------
-		// Called when this GameObject is detached from the hand
-		//-------------------------------------------------
-		private void OnDetachedFromHand( Hand hand )
-		{
-			Debug.Log("Detached from hand " + hand.name);
-		}
-
-
-    
-		//-------------------------------------------------
-		// Called when a Hand starts hovering over this object
-		//-------------------------------------------------
-		private void OnHandHoverBegin( Hand hand )
-		{
-			Debug.Log("Hovering hand: " + hand.name);
-		}
-
-
-		//-------------------------------------------------
-		// Called when a Hand stops hovering over this object
-		//-------------------------------------------------
-		private void OnHandHoverEnd( Hand hand )
-		{
-			Debug.Log("No Hand Hovering");
-		}
 
     private void HandHoverUpdate( Hand hand )
 		{
@@ -62,29 +26,13 @@ public class PuzzlePart : MonoBehaviour {
         if ( _hand == null )
         {
           _hand = hand;
-          Debug.Log("Start tracking puzzle part");
           _puzzle.TrackPuzzlePart(this, hand);
-
-          // // Call this to continue receiving HandHoverUpdate messages,
-          // // and prevent the hand from hovering over anything else
-          // hand.HoverLock( GetComponent<Interactable>() );
-
-          // // Attach this object to the hand
-          // hand.AttachObject( gameObject, attachmentFlags );
         }
         else
         {
           _hand = null;
-          Debug.Log("Release puzzle part");
           _puzzle.ReleasePuzzlePart(this, hand);
-          // // Detach this object from the hand
-          // hand.DetachObject( gameObject );
-
-          // // Call this to undo HoverLock
-          // hand.HoverUnlock( GetComponent<Interactable>() );
-
         }
       }
     }
-
 }
